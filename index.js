e9309e821b4c62f6ed21352578c235ab87634838
maimuna <mohamudmaimuna003@gmail.com>
2023-06-18 20:38:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+const leastFavoriteCustomer = "keli";
+function upperCaseCustomerName() {
+  return customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  var bestCustomer = "not bob";
+  console.log(bestCustomer);
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "maya";
+}
